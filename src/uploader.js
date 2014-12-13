c4cb9a681c0394090983fefca307c13b207fe8e7
donghui <donghui@email.com>
2014-12-13 16:12:46 +0800

    add main func

--- src/uploader.js
+++ src/uploader.js
@@ -16,35 +16,102 @@ attaches.Entities = {
   })
 };
 
-attaches.Entities = Backbone.Collection.extend({
+attaches.Entities.attaches = Backbone.Collection.extend({
   model: attaches.Entities.attach
 });
 
 attaches.Template = {
-  attachView : _.template('<span class="field-attach-name label label-primary"><%= name %></span><a href="javascript:;" class="fa fa-remove"></a>'),
-  attachesView : _.template('<div class="field-attaches"></div>')
+  attachView : _.template('<span class="field-attach-name label label-primary"><%= name %></span><a href="javascript:;" class="fa fa-remove">×</a>'),
+  attachesView : _.template('<button class="uploadBtn" type="button">上传文件</button><div class="field-attaches"></div>')
 }
 
-attaches.AttachView = Marionette.ItemView.extend({
+attaches.AttachView = Backbone.View.extend({
   className: 'field-attach',
   template: attaches.Template.attachView,
-  triggers:{
-    'click .fa-remove': 'removeAttach'
+  initialize: function(){
+    _.bindAll( this, 'onRemove', 'onProcess' );
+    this.model
+      .on('remove', this.onRemove )
+      .on('change', this.onProcess )
   },
-});
-
-attaches.AttachesView = Marionette.CompositeView.extend({
-  template:function(){
-    // TODO: 根据不同的文件类别选择不同的模板
-    return attaches.Template.attachesView;
+  render: function(){
+    this.$el.append( this.template(this.model.toJSON()) );
+    this.model.get('uploading') && this._initProcessing();
+    return this;
   },
-  childView: attaches.AttachView,
-  childViewContainer: '.field-attaches',
-  onAddChild: function(childView){
-    console.log('onAddChild')
+  onRemove: function(){
+    this.$el.remove();
+  },
+  _initProcessing: function(){
+    this.$el.append('<div class="progress">\
+      <div class="progress-bar progress-bar-success active" style="width: 50%"> 0% </div>\
+    </div>');
+  },
+  onProcess: function(){
+    if(!this.model.get('uploading')){
+      this.$el.find('.progress').remove();
+      return;
+    }
+    var progress = Math.floor(this.model.get('loaded')/this.model.get('total'))*100+'%';
+    this.$el.find('.progress-bar').css('width', progress).html(progress);
   }
 });
 
+attaches.AttachesView = Backbone.View.extend({
+  initialize: function(options){
+    _.bindAll( this, 'addOne' )
+    this.collection.on('add', this.addOne)
+    // this.render();
+  },
+  render: function(){
+    this.$el.append( attaches.Template.attachesView() );
+    this.$container = this.$el.find('.field-attaches');
+    this.collection.each( this.addOne );
+    return this;
+  },
+  addOne: function(model){
+    var view = new attaches.AttachView({ model: model });
+    this.$container.append( view.render().$el );
+  }
+});
+
+attaches.defaultOptions = {
+  multiple: true,
+  params:{},
+  url:'/devp/uploader/upload.php',
+  // 将上传成功后的文件添加到列表中
+  addUploaded: function( file ){
+    this.collection.add( file );
+  },
+  // 删除已上传的文件
+  removeUploaded: function( file_id ){
+    this.setValue( _.reject(this.getValue(), function(f){ return f.id == file_id; }) );
+  },
+  // 更新文件显示列表
+  updateList: function(files) {},
+  fileInvalid: function(name, message) {},
+  tooManyFile: function(name, message) {},
+  beforeUpload: function(uniqueId, name, data, xhr) {},
+  uploadStart: function(uniqueId, name) {},
+  uploadComplete: function(uniqueId, name, resp, status, xhr) {
+    if(status != 200) return;
+    resp = JSON.parse(resp);
+    var fileinfo = _.pick(resp,'id','name','file_size','file_type','show_name','type');
+    if( resp['file_type']=='osns_n_image') fileinfo.thumb = resp.append.thumbnails.link;
+    this.collection.get(uniqueId).clear({silent:true}).set( fileinfo );
+  },
+  uploadError: function(uniqueId, name, message) {},
+  uploadProgress: function(uniqueId, name, loaded, total) {
+    if(!this.collection.get(uniqueId)) return;
+    this.collection.get(uniqueId).set({loaded:loaded, total:total});
+  },
+  fileAdd: function(uniqueId, name, file) {
+    // 将准备上传的文件添加到Collection中
+    this.collection.add( _.extend({},file, {id:uniqueId, uploading: true}) );
+  },
+  fileRemove: function(uniqueId, name) {},
+  uploadAbort: function(uniqueId, name) {}
+}
 
 
 Form.editors.Uploader = Form.editors.Text.extend({
@@ -54,22 +121,50 @@ Form.editors.Uploader = Form.editors.Text.extend({
     'click .remove': 'removeFile'
   },
   initialize: function(options) {
+    _.extend( this, {}, attaches.defaultOptions, options.schema);
+    _.bindAll(this, 'fileAdd', 'fileRemove', 'fileInvalid', 'tooManyFile', 'beforeUpload', 'uploadStart', 'uploadAbort', 'uploadComplete', 'uploadError', 'uploadProgress');
     Form.editors.Text.prototype.initialize.call(this, options);
+
+    console.log('options',options)
+    this.collection = new attaches.Entities.attaches(this.model.get(this.key) || []);
     //Template
     this.template = options.template || this.constructor.template;
+    options.dropZone && this._initdropZone();
   },
   // getValue: function() {},
   // setValue: function(value) {}
   render: function(options) {
     Form.editors.Text.prototype.render.apply(this, arguments);
-    /*this.$el.append(this.template({
-      multiple: this.schema.multiple
-    }));*/
+    this.attaches = new attaches.AttachesView({
+      collection : this.collection
+    })
+    this.$el.append( this.attaches.render().$el )
+
+    if( this.selectButton ){
+      this.$selectButton = this.$('.uploadBtn');
+      console.log('this.$selectButton 1',this.$selectButton.length)
+      this._initSelectButton();
+    }
     return this;
   },
-  // 初始化上传按钮 ?
-  uploadFile:function(evt){
-    console.log('uploadFile', evt);
+  _initSelectButton: function( btn ){
+    console.log('this.$selectButton 2',this.$selectButton.length)
+    this.uploader = new Uploader({
+      selectButton: this.$selectButton,
+      url: this.url,
+      data: this.params
+    });
+    this.uploader
+      .on("fileAdd", this.fileAdd)
+      .on("fileRemove", this.fileRemove)
+      .on("fileInvalid", this.fileInvalid)
+      .on("tooManyFile", this.tooManyFile)
+      .on("beforeUpload", this.beforeUpload)
+      .on("uploadStart", this.uploadStart)
+      .on("uploadProgress", this.uploadProgress)
+      .on("uploadAbort", this.uploadAbort)
+      .on("uploadComplete", this.uploadComplete)
+      .on("uploadFail", this.uploadError);
   }
 },{
   template: _.template('\
@@ -77,124 +172,6 @@ Form.editors.Uploader = Form.editors.Text.extend({
   ', null, Form.templateSettings),
 });
 
-
-Form.editors.Filepicker = Form.editors.Text.extend({
-    tagName: 'div',
-
-    events: {
-        'change input[type=file]': 'uploadFile',
-        'click .remove': 'removeFile'
-    },
-
-    initialize: function(options) {
-        _.bindAll(this, 'filepickerSuccess', 'filepickerError', 'filepickerProgress');
-        editors.Text.prototype.initialize.call(this, options);
-        this.$input = $('<input type="hidden" name="'+this.key+'" />');
-        this.$uploadInput = $('<input type="file" multiple="multiple" />');
-        this.$loader = $('<p class="upload-status"><span class="loader"></span> Uploading&hellip;</p>');
-        this.$error = $('<p class="upload-error error">Error</p>');
-        this.$list = $('<ul class="file-list">');
-    },
-
-    // return an array of file dicts
-    getValue: function() {
-        var val = this.$input.val();
-        return val ? JSON.parse(val) : [];
-    },
-
-    setValue: function(value) {
-        var str, files = value;
-        if (_(value).isObject()) {
-            str = JSON.stringify(value);
-        } else {
-            files = value ? JSON.parse(value) : [];
-        }
-        this.$input.val(str);
-        this.updateList(files);
-    },
-
-    render: function(options) {
-        editors.Text.prototype.render.apply(this, arguments);
-
-        this.$el.append(this.$input);
-        this.$el.append(this.$uploadInput);
-        this.$el.append(this.$loader.hide());
-        this.$el.append(this.$error.hide());
-        this.$el.append(this.$list);
-        return this;
-    },
-
-    uploadFile: function() {
-        var s3upload = new S3Upload({
-            file_dom_selector: this.$uploadInput,
-            s3_sign_put_url: '/sign_s3_put/',
-            onProgress: this.filepickerProgress,
-            onFinishS3Put: this.filepickerSuccess,
-            onError: this.filepickerError
-        });
-    },
-
-    filepickerSuccess: function(s3Url, file) {
-        console.log('File uploaded', s3Url);
-        this.$loader.hide();
-        this.$error.hide();
-        this.$uploadInput.val('');
-
-        var newFiles = [{
-            url: s3Url,
-            filename: file.name,
-            size: file.size,
-            content_type: file.type
-        }];
-
-        console.log('File uploaded (processed)', newFiles);
-        this.setValue(this.getValue().concat(newFiles));
-    },
-
-    filepickerError: function(msg, file) {
-        console.debug('Filepicker error', msg);
-        this.$loader.hide();
-        this.$error.show();
-    },
-
-    filepickerProgress: function(percent, message, publicUrl, file) {
-        //console.log('Filepicker progress', percent, message);
-        this.$loader.show();
-        this.$error.hide();
-    },
-
-    updateList: function(files) {
-        // this code is currently duplicated as a handlebar helper (I wanted to let this
-        // backbone-forms field stand on its own)
-        var displayFilesize = function(bytes) {
-            // TODO improve this function
-            return Math.floor(bytes / 1024) + 'K';
-        };
-
-        this.$list.empty();
-        _(files).each(function(file) {
-            var a = $('<a>', {
-                target: '_blank',
-                href: file.url,
-                text: file.filename + ' (' + file.content_type + ') ' + displayFilesize(file.size)
-            });
-            var li = $('<li>').append(a);
-            li.append(a, ' ', $('<a href="#" class="remove"><i class="icon-remove"></i></a>').data('url', file.url));
-            this.$list.append(li);
-        }, this);
-
-        this.$list[files.length ? 'show' : 'hide']();
-    },
-
-    removeFile: function(ev) {
-        if (ev) ev.preventDefault();
-        var url = $(ev.currentTarget).data('url');
-        var files = this.getValue();
-        this.setValue(_(files).reject(function(one) {
-            return one.url === url;
-        }));
-    }
-
-});
+Form.attaches = attaches;
 
 })(window || global || this);
